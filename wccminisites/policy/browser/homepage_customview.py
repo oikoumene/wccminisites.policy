db56cb0ff5eee93566b99052fd14dce85890e6b7
glenn <glenn@glenn-Lenovo-G480>
2016-02-18 22:22:30 +0800

    added homepage custom view

---
+++ wccminisites/policy/browser/homepage_customview.py
@@ -0,0 +1,22 @@
+from five import grok
+from Products.CMFCore.utils import getToolByName
+from Products.ATContentTypes.interface import IATFolder
+from Products.CMFCore.interfaces import ISiteRoot
+from wccminisites.policy.interfaces import IProductSpecific
+
+grok.templatedir('templates')
+
+class homepage_customview(grok.View):
+    grok.context(ISiteRoot)
+    grok.require('zope2.View')
+    grok.layer(IProductSpecific)
+
+    @property
+    def catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+    
+    def contents(self):
+        return self.catalog.unrestrictedSearchResults(portal_type='News Item',
+                                                      sort_on='created',
+                                                      sort_order='reverse',
+                                                      review_state='published')[:4]
