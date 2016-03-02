65173af5a4635e3f44e486d672a75b9f0d274c58
glenn <glenn@glenn-Lenovo-G480>
2016-03-02 22:53:53 +0800

    added new browser view to display members under network group as per request

---
+++ wccminisites/policy/browser/member_listing_view.py
@@ -0,0 +1,37 @@
+from five import grok
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.interfaces import IContentish
+from plone.app.discussion.interfaces import IConversation, IComment
+from Products.CMFPlone.PloneBatch import Batch
+from wccminisites.policy.interfaces import IProductSpecific
+
+grok.templatedir('templates')
+
+class member_listing_view(grok.View):
+    grok.context(IContentish)
+    grok.require('zope2.View')
+    grok.layer(IProductSpecific)
+    
+    @property
+    def portal_membership(self):
+        return getToolByName(self.context, 'portal_membership')
+    
+    @property
+    def portal_groups(self):
+        return getToolByName(self.context, 'portal_groups')
+    
+    def network_group(self):
+        results = []
+        groups = self.portal_groups.getGroupById('network')
+        if groups:
+            for gr in groups.getGroupMembers():
+                data = {}
+                member = self.portal_membership.getMemberById(gr.getUserName())
+                data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                data['fullname'] = member.getProperty('fullname')
+                data['username'] = gr.getUserName()
+                results.append(data)
+        
+        if results:
+            results.sort(key = lambda x :x['fullname'])
+        return results
