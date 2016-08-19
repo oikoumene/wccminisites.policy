7c45364fa1753fbbc295215f27c23a5310a7dec1
glenn <glenn@glenn-Lenovo-G480>
2016-08-19 14:31:37 +0800

    added classes

--- wccminisites/policy/interfaces.py
+++ wccminisites/policy/interfaces.py
@@ -1,5 +1,15 @@
 from zope.interface import Interface
+from wccminisites.policy import MessageFactory as _
+from quintagroup.formlib.captcha import Captcha
 
 class IProductSpecific(Interface):
     pass
 
+class IExtendRegistrationForm(Interface):
+    """Marker interface for my custom registration form
+    """
+
+class ICaptchaSchema(Interface):
+    captcha = Captcha(
+        title=_(u'Type the code'),
+        description=_(u'Type the code from the picture shown below.'))
