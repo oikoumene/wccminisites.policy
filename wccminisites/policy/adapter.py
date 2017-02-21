973ee91308b0c1a4c7c37642df6a1aff8c7f8bb6
glenn <glenn@glenn-Lenovo-G480>
2017-02-21 23:43:29 +0800

    added new fields

--- wccminisites/policy/adapter.py
+++ wccminisites/policy/adapter.py
@@ -5,6 +5,24 @@ from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
 class EnhancedUserDataSchemaAdapter(UserDataPanelAdapter):
     #schema = IEnhancedUserDataSchema
     
+    def get_church(self):
+        return self.context.getProperty('church', '')
+    def set_church(self, value):
+        return self.context.setMemberProperties({'church': value})
+    church = property(get_church, set_church)
+    
+    def get_region(self):
+        return self.context.getProperty('region', '')
+    def set_region(self, value):
+        return self.context.setMemberProperties({'region': value})
+    region = property(get_region, set_region)
+    
+    def get_wcc_commission(self):
+        return self.context.getProperty('wcc_commission', '')
+    def set_wcc_commission(self, value):
+        return self.context.setMemberProperties({'wcc_commission': value})
+    wcc_commission = property(get_wcc_commission, set_wcc_commission)
+    
     def get_twitter_username(self):
         return self.context.getProperty('twitter_username', '')
     def set_twitter_username(self, value):
