32a1b9c237aada141e5d6aa36a3e75ea7bfbb54b
glenn <glenn@glenn-Lenovo-G480>
2017-02-24 01:33:57 +0800

    added codes to query based on search results

--- wccminisites/policy/browser/member_listing_view.py
+++ wccminisites/policy/browser/member_listing_view.py
@@ -26,15 +26,89 @@ class member_listing_view(grok.View):
         results = []
         groups = self.portal_groups.getGroupById('network')
         if groups:
-            for gr in groups.getGroupMembers():
+            if self.request.form:
+                search_by = self.request.form['search_by']
+                keyword = self.request.form['keyword'].lower() or ''
                 
-                member = self.portal_membership.getMemberById(gr.getUserName())
-                if member:
-                    data = {}
-                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
-                    data['fullname'] = member.getProperty('fullname')
-                    data['username'] = gr.getUserName()
-                    results.append(data)
+                if keyword:
+                    
+                    if search_by == 'Name':
+                        for gr in groups.getGroupMembers():
+                        
+                            member = self.portal_membership.getMemberById(gr.getUserName())
+                            if member and member.getProperty('fullname'):
+                                
+                                if keyword in member.getProperty('fullname').lower():
+                                    data = {}
+                                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                                    data['fullname'] = member.getProperty('fullname')
+                                    data['username'] = gr.getUserName()
+                                    results.append(data)
+                                        
+                    elif search_by == 'Church':
+                        for gr in groups.getGroupMembers():
+                        
+                            member = self.portal_membership.getMemberById(gr.getUserName())
+                            if member and member.getProperty('church'):
+                                
+                                if keyword in member.getProperty('church').lower():
+                                    data = {}
+                                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                                    data['fullname'] = member.getProperty('fullname')
+                                    data['username'] = gr.getUserName()
+                                    results.append(data)
+                    elif search_by == 'Region':
+                        for gr in groups.getGroupMembers():
+                        
+                            member = self.portal_membership.getMemberById(gr.getUserName())
+                            if member and member.getProperty('region'):
+                                
+                                if keyword in member.getProperty('region'):
+                                    data = {}
+                                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                                    data['fullname'] = member.getProperty('fullname')
+                                    data['username'] = gr.getUserName()
+                                    results.append(data)
+                    elif search_by == 'Country':
+                        for gr in groups.getGroupMembers():
+                        
+                            member = self.portal_membership.getMemberById(gr.getUserName())
+                            if member and member.getProperty('location'):
+                                
+                                if keyword in member.getProperty('location'):
+                                    data = {}
+                                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                                    data['fullname'] = member.getProperty('fullname')
+                                    data['username'] = gr.getUserName()
+                                    results.append(data)
+                    elif search_by == 'WCC Commission':
+                        for gr in groups.getGroupMembers():
+                        
+                            member = self.portal_membership.getMemberById(gr.getUserName())
+                            if member and member.getProperty('wcc_commission'):
+                                
+                                if keyword in (','.join(member.getProperty('wcc_commission'))).lower():
+                                    data = {}
+                                    data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                                    data['fullname'] = member.getProperty('fullname')
+                                    data['username'] = gr.getUserName()
+                                    results.append(data)
+                                    
+                            
+                            
+                                    
+            
+            else:
+            
+                for gr in groups.getGroupMembers():
+                    
+                    member = self.portal_membership.getMemberById(gr.getUserName())
+                    if member:
+                        data = {}
+                        data['img'] = self.portal_membership.getPersonalPortrait(gr.getUserName()).absolute_url()
+                        data['fullname'] = member.getProperty('fullname')
+                        data['username'] = gr.getUserName()
+                        results.append(data)
         
         if results:
             results.sort(key = lambda x :x['fullname'])
